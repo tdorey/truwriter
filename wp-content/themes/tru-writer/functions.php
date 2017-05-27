1c32e11ae328d530d346703c812f8852af7b43e9
Tanya Dorey <telias@tru.ca>
2017-05-26 23:26:06 -0700

    Removed creativve commons functions

--- wp-content/themes/tru-writer/functions.php
+++ wp-content/themes/tru-writer/functions.php
@@ -546,64 +546,6 @@ function truwriter_publish ( $post ) {
 
 add_action(  'publish_post',  'truwriter_publish', 10, 2 );
 
-# -----------------------------------------------------------------
-# Creative Commons Licensing
-# -----------------------------------------------------------------
-
-
-function cc_license_html ($license, $author='', $yr='') {
-	// outputs the proper license
-	// $license is abbeviation. author is from post metadata, yr is from post date
-	
-	if ( !isset( $license ) or $license == '' ) return '';
-	
-	if ($license == 'copyright') {
-		// boo copyrighted! sigh, slap on the copyright text. Blarg.
-		return 'This work by ' . $author . ' is &copy;' . $yr . ' All Rights Reserved';
-	} 
-	
-	// names of creative commons licenses
-	$commons = array (
-		'by' => 'Attribution',
-		'by-sa' => 'Attribution-ShareAlike',
-		'by-nd' => 'Attribution-NoDerivs',
-		'by-nc' => 'Attribution-NonCommercial',
-		'by-nc-sa' => 'Attribution-NonCommercial-ShareAlike',
-		'by-nc-nd' => 'Attribution-NonCommercial-NoDerivs',
-	);
-	
-	// do we have an author?
-	$credit = ($author == '') ? '' : ' by ' . $author;
-	
-	return '<a rel="license" href="http://creativecommons.org/licenses/' . $license . '/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/' . $license . '/4.0/88x31.png" /></a><br />This work' . $credit . ' is licensed under a <a rel="license" href="http://creativecommons.org/licenses/' . $license . '/4.0/">Creative Commons ' . $commons[$license] . ' 4.0 International License</a>.';            
-}
-
-
-function cc_license_select_options ($curr) {
-	// output for select form options for use in forms
-
-	$str = '';
-	
-	// to restrict the list of options, comment out lines you do not want
-	// to make available (HACK HACK HACK)
-	$licenses = array (
-		'by' => 'Creative Commons Attribution',
-		'by-sa' => 'Creative Commons Attribution-ShareAlike',
-		'by-nd' => 'Creative Commons Attribution-NoDerivs',
-		'by-nc' => 'Creative Commons Attribution-NonCommercial',
-		'by-nc-sa' => 'Creative Commons Attribution-NonCommercial-ShareAlike',
-		'by-nc-nd' => 'Creative Commons Attribution-NonCommercial-NoDerivs',
-		'copyright' => 'Copyrighted All Rights Reserved',
-	);
-	
-	foreach ($licenses as $key => $value) {
-		// build the striing of select options
-		$selected = ( $key == $curr ) ? ' selected' : '';
-		$str .= '<option value="' . $key . '"' . $selected  . '>' . $value . '</option>';
-	}
-	
-	return ($str);
-}
 
 # -----------------------------------------------------------------
 # Useful spanners and wrenches
