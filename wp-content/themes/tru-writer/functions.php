9b722d87be09452b459ff0cebc75e78d0939994c
Tanya Dorey <telias@tru.ca>
2017-05-28 21:04:56 -0700

    Trying to add sidebar

--- wp-content/themes/tru-writer/functions.php
+++ wp-content/themes/tru-writer/functions.php
@@ -570,6 +570,36 @@ function wpb_alter_comment_form_fields($fields) {
 add_filter('comment_form_default_fields', 'wpb_alter_comment_form_fields');
 
 # -----------------------------------------------------------------
+# Adding a sidebar
+# -----------------------------------------------------------------
+function wpb_widgets_init() {
+
+    register_sidebar( array(
+        'name' => __( 'Main Sidebar', 'wpb' ),
+        'id' => 'sidebar-1',
+        'description' => __( 'The main sidebar appears on the right on each page except the front page template', 'wpb' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+
+    register_sidebar( array(
+        'name' =>__( 'Front page sidebar', 'wpb'),
+        'id' => 'sidebar-2',
+        'description' => __( 'Appears on the static front page template', 'wpb' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+}
+
+add_action( 'widgets_init', 'wpb_widgets_init' );
+
+
+
+# -----------------------------------------------------------------
 # Useful spanners and wrenches
 # -----------------------------------------------------------------
 
