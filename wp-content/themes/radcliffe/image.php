b8d08dcdc3177c2820c555fa03cb1e2da94375e6
Tanya Dorey <telias@tru.ca>
2017-05-29 21:24:27 -0700

    image  - add div get

--- wp-content/themes/radcliffe/image.php
+++ wp-content/themes/radcliffe/image.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
+    </div>
+<?php get_sidebar(); ?>
+    </div>
+
 <div class="content">
 											        
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
