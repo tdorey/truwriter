284270081dc1a099b292a91fa386870c18e23bf6
Tanya Dorey <telias@tru.ca>
2017-05-29 20:57:52 -0700

    sinlge move get sidebar

--- wp-content/themes/radcliffe/single.php
+++ wp-content/themes/radcliffe/single.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<?php get_sidebar(); ?>
-
 <div class="content">
+
+    <?php get_sidebar(); ?>
 											        
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
