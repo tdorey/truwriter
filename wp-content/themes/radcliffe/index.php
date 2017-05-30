eb3d0c55c3f4218dd8f6b270055f5313efdb343b
Tanya Dorey <telias@tru.ca>
2017-05-29 21:42:16 -0700

    index add main div

--- wp-content/themes/radcliffe/index.php
+++ wp-content/themes/radcliffe/index.php
@@ -2,19 +2,19 @@
 
     <div id="main">
 
-<div class="content">
+        <div class="content">
 
-	<?php if (have_posts()) : ?>
+	    <?php if (have_posts()) : ?>
 	
-		<div class="posts">
+		    <div class="posts">
 	
-			<?php
-			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-			$total_post_count = wp_count_posts();
-			$published_post_count = $total_post_count->publish;
-			$total_pages = ceil( $published_post_count / $posts_per_page );
+			    <?php
+			    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			    $total_post_count = wp_count_posts();
+			    $published_post_count = $total_post_count->publish;
+			    $total_pages = ceil( $published_post_count / $posts_per_page );
 			
-			if ( "1" < $paged ) : ?>
+			    if ( "1" < $paged ) : ?>
 			
 				<div class="page-title section small-padding">
 				
@@ -26,38 +26,37 @@
 			
 			<?php endif; ?>
 				
-		    	<?php while (have_posts()) : the_post(); ?>
+		    	    <?php while (have_posts()) : the_post(); ?>
 		    	
-					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		    	
 			    		<?php get_template_part( 'content', get_post_format() ); ?>
 			    				    		
 		    		</div> <!-- /post -->
 		    			        		            
-		        <?php endwhile; ?>
+		            <?php endwhile; ?>
 	        	        		
-			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+			    <?php if ( $wp_query->max_num_pages > 1 ) : ?>
 			
-				<div class="archive-nav">
+				    <div class="archive-nav">
 				
-					<?php echo get_next_posts_link( '&laquo; ' . __('Older posts', 'radcliffe')); ?>
+					    <?php echo get_next_posts_link( '&laquo; ' . __('Older posts', 'radcliffe')); ?>
 						
-					<?php echo get_previous_posts_link( __('Newer posts', 'radcliffe') . ' &raquo;'); ?>
+					    <?php echo get_previous_posts_link( __('Newer posts', 'radcliffe') . ' &raquo;'); ?>
 					
 					<div class="clear"></div>
 					
-				</div> <!-- /post-nav archive-nav -->
+				    </div> <!-- /post-nav archive-nav -->
 								
-			<?php endif; ?>
+			    <?php endif; ?>
         	                    
-		<?php endif; ?>
+		        <?php endif; ?>
 		
-	</div> <!-- /posts -->
+	        </div> <!-- /posts -->
 		
-</div> <!-- /content section-inner -->
+        </div> <!-- /content section-inner -->
 
-    </div>
-<?php get_sidebar(); ?>
+        <?php get_sidebar(); ?>
     </div>
 
 <?php get_footer(); ?>
