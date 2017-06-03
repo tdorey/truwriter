e757574b1c19183ee74fe194c215d6978f90a1ad
Tanya Dorey <telias@tru.ca>
2017-06-02 22:28:32 -0700

    add sidebar functions

--- wp-content/themes/radcliffe/sidebar.php
+++ wp-content/themes/radcliffe/sidebar.php
@@ -1,5 +1,5 @@
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-    <div id="secondary" class="widget-area" role="complementary">
+    <div id="secondary" class="widget_areas" role="complementary">
         <?php dynamic_sidebar( 'sidebar-1' ); ?>
     </div><!-- #secondary -->
 <?php endif; ?>
